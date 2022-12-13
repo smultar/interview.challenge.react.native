4913952045a0dff08ee2e4bf30ca3cae078fbd27
Smultar <Jason@smultar.com>
2022-12-13 12:58:38 -0500

    Added a few more icons

--- assets/icons.js
+++ assets/icons.js
@@ -9,7 +9,17 @@ const icons = {
     compass: require('./icons/compass.png'),
     school: require('./icons/school.png'),
     location: require('./icons/location.png'),
-    sun: require('./icons/sun.png')
+    sun: require('./icons/sun.png'),
+    email: require('./icons/email.png'),
+    phone: require('./icons/phone.png'),
+    website: require('./icons/website.png'),
+    students: require('./icons/students.png'),
+    attendance: require('./icons/attendance.png'),
+    award: require('./icons/award.png'),
+    math: require('./icons/math.png'),
+    reading: require('./icons/reading.png'),
+    writing: require('./icons/writing.png'),
+    sports: require('./icons/sports.png'),
 };
 
 export default icons;
