d8e63e86d2ed0dba20e6da9481c81123e3fed4c1
Smultar <Jason@smultar.com>
2022-12-13 14:40:20 -0500

    Adjusted icon resolutions and added phone.

--- assets/icons.js
+++ assets/icons.js
@@ -1,10 +1,6 @@
 // Manual Imports
 // Got to work on an automated way to pull in all the icons from a folder
 
-import compass from './icons/compass.png';
-import school from './icons/school.png';
-import location from './icons/location.png';
-
 const icons = {
     compass: require('./icons/compass.png'),
     school: require('./icons/school.png'),
@@ -13,7 +9,6 @@ const icons = {
     email: require('./icons/email.png'),
     phone: require('./icons/phone.png'),
     website: require('./icons/website.png'),
-    students: require('./icons/students.png'),
     attendance: require('./icons/attendance.png'),
     award: require('./icons/award.png'),
     math: require('./icons/math.png'),
